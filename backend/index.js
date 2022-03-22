66ef64b419840d4d35fa054ee596dfa40482e33f
Md. Rafiuzzaman Bhuiyan Afridi <afridirk10@gmail.com>
2022-03-22 20:56:32 +0600

    filter added

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 const uri =
-  "mongodb+srv://rafi_atlas:QiNUG38CTNbZm3f@cluster0.j9yy4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  "mongodb+srv://{your own credentials}@cluster0.j9yy4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -22,13 +22,21 @@ async function run() {
     await client.connect();
     const database = client.db("blog");
     const blogCollection = database.collection("articles");
-    console.log("hit");
+    console.log("connected");
     // GET API
     app.get("/blogs", async (req, res) => {
       const cursor = blogCollection.find({});
       const articles = await cursor.toArray();
       console.log("getting all");
       res.send(articles);
+    });
+    // filter by category 
+    app.get("/category", async (req, res) => {
+      const type = req.query.category;
+      console.log(type);
+      const cursor = blogCollection.find({ type : type});
+      const service = await cursor.toArray();
+      res.send(service);
     });
     // DELETE API
     app.delete("/blogs", async (req, res) => {
